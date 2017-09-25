c41dd09af8a6bf69c870348db28bc5ed19de23cc
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-25 14:16:59 +0100

    Creating components manully and CLI, working with string interpolation

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-success-alert',
+    templateUrl: './success-alert.component.html',
+    styles: [`
+        p {
+            padding: 20px;
+            background-color: green;
+            border: 1px solid blue;
+        }
+    `]
+})
+export class SuccessAlertComponent {
+
+}
